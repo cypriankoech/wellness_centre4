dda21f9de21f847cadd51cfe85c8ba54e86327d0
cypriankoech <cypriankibet46@gmail.com>
2021-09-10 21:06:17 +0300

    Welcome Page

--- src/main/java/com/oopii/wellness_centre4/WelcomePage.java
+++ src/main/java/com/oopii/wellness_centre4/WelcomePage.java
@@ -11,9 +11,11 @@ public class WelcomePage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WelcomePage.class.getResource("welcome-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Wellness Centre");
         stage.setScene(scene);
+        stage.setResizable(false);
+
         stage.show();
     }
 
